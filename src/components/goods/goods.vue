fdeb247c9b49c6ff6038f52f607b0b5ffa74feab
MagerChan <1061340144@qq.com>
2017-11-05 23:24:52 +0800

    购物车模块和加购物车模块

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
 	<div class="goods">
 		<div class="menu-wrapper" ref="menuWrapper">
 			<ul>
-			<li v-for="item in goods" class="menu-item">
+			<li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex === index}" @click="selectMenu(index,$event)">
 				<span class="text border-1px">
 					<span v-show="item.type>0" class="icon" :class="classMap[item.type]">
 					</span>
@@ -13,7 +13,7 @@
 		</div>
 		<div class="foods-wrapper" ref="foodsWrapper">
 			<ul>
-				<li v-for="item in goods" class="food-list">
+				<li v-for="item in goods" class="food-list food-list-hook">
 					<h1 class="title">{{item.name}}</h1>
 					<ul>
 						<li v-for="food in item.foods" class="food-item border-1px">
@@ -24,12 +24,14 @@
 								<h2 class="name">{{food.name}}</h2>
 								<p class="desc">{{food.description}}</p>
 								<div class="extra">
-									<span class="count">月售{{food.sellCount}}份</span>
-									<span>好评率{{food.rating}}%</span>
+									<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
 								</div>
 								<div class="price">
 									<span class="now">￥{{food.price}}</span>
 									<span v-show="food.oldPrice" class="old">{{food.oldPrice}}</span>
+								</div>
+								<div class="cartcontrol-wrapper">
+									<cartcontrol :food="food"></cartcontrol>
 								</div>
 							</div>
 						</li>
@@ -37,11 +39,14 @@
 				</li>
 			</ul>
 		</div>
+		<shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
+  import shopcart from '../shopcart/shopcart';
+  import cartcontrol from '../cartcontrol/cartcontrol';
   const ERR_OK = 0;
 
   export default {
@@ -52,8 +57,33 @@
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      },
+      selectFoods() {
+        let foods = [];
+        this.goods.forEach((good) => {
+          good.foods.forEach((food) => {
+            if (food.count) {
+              foods.push(food);
+            }
+          });
+        });
+        return foods;
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -63,16 +93,48 @@
           this.goods = response.data;
           this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
           });
         }
       });
     },
     methods: {
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
       _initScroll() {
-        this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        });
 
-        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3
+        });
+
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
       }
+    },
+    components: {
+      shopcart,
+      cartcontrol
     }
   };
 </script>
@@ -97,6 +159,14 @@
 				width:56px
 				padding:0 12px
 				line-height:14px
+				&.current
+					position:relative
+					z-index:10
+					margin-top:-1px
+					background:#fff
+					font-weight:700
+					.text
+						border-none()
 				.icon
 					display:inline-block
 					vertical-align:top
@@ -136,7 +206,7 @@
 				margin:18px
 				padding-bottom:18px
 				border-1px(rgba(7,17,28,0.1))
-				&.last-child
+				&:last-child
 					border-none()
 					margin-bottom:0px
 				.icon
@@ -155,6 +225,7 @@
 						font-size:10px
 						color:rgb(147,153,159)
 					.desc
+						line-height:12px
 						margin-bottom:8px
 					.extra
 						.count
@@ -170,6 +241,10 @@
 							text-decoration:line-through
 							font-size:10px
 							color:rgb(147,153,159)
+					.cartcontrol-wrapper
+						position:absolute
+						right:0
+						bottom:12px
 
 
 
